e886ac668c58e2d43908f7816c70e05d0182b6d9
Brandon Dekker <bdekker2@email.davenport.edu>
2019-01-27 18:21:35 -0500

    moving objects

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private Vector3 mouseOrigin;
+    private float panSpeed = 1.0f;
+
+
+
+    private bool isPanning = false;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // Get the right mouse button
+        if (Input.GetMouseButtonDown(1))
+        {
+            // Get mouse origin
+            mouseOrigin = Input.mousePosition;
+            isPanning = true;
+        }
+
+        // Disable movements on button release
+        if (!Input.GetMouseButton(1)) isPanning = false;
+
+        // Move the camera on it's XY plane
+        if (isPanning)
+        {
+            Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - mouseOrigin);
+
+            Vector3 move = new Vector3(pos.x * panSpeed, pos.y * panSpeed, 0);
+            transform.Translate(move, Space.Self);
+        }
+
+       
+    }
+}
