aba71ecd4b81659a69b3162096f6c46c883a9723
Brandon Dekker <bdekker2@email.davenport.edu>
2019-01-27 19:59:43 -0500

    gravity, collision, thrust
    added gravity similar to the gravity on the moon. made collisions more reliable. added a thrust script that can be edited in the unity inspector. apply thrust using the space bar. thrust must be able to move the object it is applied to based on the mass of the object and the gravity of the environment.

---
+++ Assets/Scripts/ThrustCreator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrustCreator : MonoBehaviour
+{
+
+    public bool applyThrust = false;
+    public float thrust;
+    public Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            applyThrust = true;
+        }
+        else
+        {
+            applyThrust = false;
+        }
+        if (applyThrust)
+        {
+            rb.AddForce(transform.up * thrust);
+        }
+    }
+    // Start is called before the first frame update
+   
+    // Update is called once per frame
+
+}
