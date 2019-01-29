61a15f68d51ccd0d32e2e8ef279e16b0b494ab4b
Brandon Dekker <bdekker2@email.davenport.edu>
2019-01-29 16:52:24 -0500

    Cube rotation thrusting
    rotations added. a new cube has been added to apply the rotations and thrust too. the ball is unstable because it rolls. the cube now lifts off, rotates, and moves based on thrust and the direction it is facing from the rotations. the cube is followed by the camera.
    
    fuel tank needs added
    rotation locks to prevent too much rotation needs added

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public bool right = false, left = false, forward = false, backward = false;
+    public float RotationMultiplier = 3.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        right = false;
+        left = false;
+        forward = false;
+        backward = false;
+        if (Input.GetKey(KeyCode.W))
+        {
+            forward = true;
+            transform.Rotate(Vector3.forward * Time.deltaTime * RotationMultiplier);
+        }
+        if (Input.GetKey(KeyCode.S)) 
+        {
+            backward = true;
+            transform.Rotate(Vector3.back * Time.deltaTime * RotationMultiplier);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            left = true;
+            transform.Rotate(Vector3.left * Time.deltaTime * RotationMultiplier);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            right = true;
+            transform.Rotate(Vector3.right * Time.deltaTime * RotationMultiplier);
+        }
+
+        if (forward)
+        {
+            transform.Rotate(Vector3.up * Time.deltaTime);
+        }
+        if (backward)
+        {
+            transform.Rotate(Vector3.down * Time.deltaTime);
+        }
+        if (left)
+        {
+            transform.Rotate(Vector3.left * Time.deltaTime);
+        }
+        if (right)
+        {
+            transform.Rotate(Vector3.right * Time.deltaTime);
+        }
+    }
+}
--- Assets/Scripts/ThrustCreator.cs
+++ Assets/Scripts/ThrustCreator.cs
@@ -1,10 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+/// <summary>
+/// attach this script to the object that is having thrust applied to it. thrust must counter act the gravity.
+/// change the thrust button by changing the getkey keycode 
+/// 
+/// </summary>
 
 public class ThrustCreator : MonoBehaviour
 {
-
+    //change the thrust in the unity editor to match the mass of the object being moved
     public bool applyThrust = false;
     public float thrust;
     public Rigidbody rb;
@@ -16,6 +21,12 @@ public class ThrustCreator : MonoBehaviour
 
     void FixedUpdate()
     {
+        // used to reset the position of an object if it gets lost. for testing purposes only
+        if (Input.GetKey(KeyCode.R))
+        {
+            transform.position = new Vector3(0.0f, 2.0f, 0.0f);
+            transform.rotation = Quaternion.identity;
+        }
         if (Input.GetKey(KeyCode.Space))
         {
             applyThrust = true;
